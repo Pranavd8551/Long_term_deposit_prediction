60ced25f6a83fde1635c5f5d71a2e75856a295ce
Pranav <pranavdeshmukh4545@gmail.com>
2024-09-22 17:23:42 +0530

    All dependencies installed

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Long_term_deposit_prediction",
+    version="0.0.0",
+    author="Pranav",
+    author_email="pranavdeshmukh4545.@gmail.com",
+    packages=find_packages()
+)
