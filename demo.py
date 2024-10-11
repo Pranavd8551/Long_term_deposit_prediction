647c52573b0cc349bfc1e8d31a54b2b010a03b89
Pranav <pranavdeshmukh4545@gmail.com>
2024-10-11 17:48:26 +0530

    Data ingestion completed

--- demo.py
+++ demo.py
@@ -1,6 +1,4 @@
-from long_term_deposit_prediction.exception import DepositException
-import sys
-try:
-    a=2/0
-except Exception as e:
-    raise DepositException(e,sys)
+from long_term_deposit_prediction.pipline.training_pipeline import TrainPipeline
+
+obj = TrainPipeline()
+obj.run_pipeline()
